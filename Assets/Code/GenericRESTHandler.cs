9957109220dea1c9a9db9245b79848bd9622a81d
Keith Thomas <kryzodoze@gmail.com>
2016-08-23 13:56:35 +0900

    Create Unity Project Closes #128703077 - Create initial unity project with StrangeIOC and RestSharp included.

---
+++ Assets/Code/GenericRESTHandler.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericRESTHandler : MonoBehaviour
+{
+    private string results;
+
+    // Use this for initialization
+    void Start()
+    {
+        results = "No results gathered so far.";
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    public string Results
+    {
+        get
+        {
+            return results;
+        }
+    }
+
+    public WWW GET(string url, System.Action onComplete)
+    {
+
+        WWW www = new WWW(url);
+        StartCoroutine(WaitForRequest(www, onComplete));
+        return www;
+    }
+
+    public WWW POST(string url, string fieldName, string value, System.Action onComplete)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField(fieldName, value);
+
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(WaitForRequest(www, onComplete));
+        return www;
+    }
+
+    private IEnumerator WaitForRequest(WWW www, System.Action onComplete)
+    {
+        yield return www;
+
+        if (www.error == null)
+        {
+            results = www.text;
+            onComplete();
+        }
+        else
+        {
+            Debug.Log(www.error);
+        }
+    }
+}
---
+++ Assets/Code/RootMain.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using strange.extensions.context.impl;
+
+public class RootMain : ContextView {
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Instantiate the context, passing it this instance.
+        context = new RootContext(this);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
