bd69ff1bfe30c3ce887974ba7af82c3d86b40eba
IanSwift <ian.p.swift@gmail.com>
2016-09-15 15:23:41 -0500

    Add create game network call

---
+++ Assets/Code/CreateGameNetworkCall.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class CreateGameNetworkCall : MonoBehaviour {
+
+	public InputField field;
+	string baseUrl;
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_EDITOR
+			baseUrl = "http://localhost:8080";
+		#elif UNITY_WEBGL
+			baseUrl = "http://supply-attack-server.herokuapp.com";
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public IEnumerator upload() {
+		UnityWebRequest webRequest = new UnityWebRequest (baseUrl + "/game", UnityWebRequest.kHttpVerbPOST);
+		UploadHandler uploadHandler = new UploadHandlerRaw (Encoding.UTF8.GetBytes("{\"gameName\":\""+field.text+"\"}"));
+		DownloadHandler downloadHandler = new DownloadHandlerBuffer ();
+		webRequest.uploadHandler = uploadHandler;
+		webRequest.downloadHandler = downloadHandler;
+		webRequest.SetRequestHeader ("Content-Type", "application/json");
+
+		yield return webRequest.Send ();
+	}
+
+	public void onClick() {
+		StartCoroutine(upload ());
+		return;
+	}
+}
---
+++ Assets/Code/LaunchCreateGameScreen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LaunchCreateGameScreen : MonoBehaviour {
+
+	public static int SCENE_CREATE_GAME = 1;
+
+	public void onClick() {
+		SceneManager.LoadScene(SCENE_CREATE_GAME);
+	}
+}
