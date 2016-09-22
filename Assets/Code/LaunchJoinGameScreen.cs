2255204bb9a79c54e96cd57177cb140b9f4935e5
JmBirk <JmBirk@gmail.com>
2016-09-22 16:44:09 -0500

    Added Script to MainMenuScene Join Game button

---
+++ Assets/Code/LaunchJoinGameScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LaunchJoinGameScreen : MonoBehaviour {
+
+    public static int SCENE_JOIN_GAME = 4;
+
+
+    public void onClick()
+    {
+        SceneManager.LoadScene(SCENE_JOIN_GAME);
+
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
