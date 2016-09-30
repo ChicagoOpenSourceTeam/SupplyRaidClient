3c8356f23b96ee55bf6ed594cb4968a81cb12425
JmBirk <JmBirk@gmail.com>
2016-09-29 19:41:09 -0500

    Lobby makes repeated GET call to players.

---
+++ Assets/Code/GetPlayersFromServer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
+
+public class GetPlayersFromServer : MonoBehaviour {
+
+    string baseUrl;
+    public GameObject errorDialog;
+    private DisplayErrorDialog displayErrorDialog;
+
+    // Use this for initialization
+    void Start () {
+#if UNITY_EDITOR
+        baseUrl = "http://localhost:8080";
+#elif UNITY_WEBGL
+        baseUrl = "http://supply-attack-server.herokuapp.com";
+#endif
+
+
+        StartCoroutine(MakeGETCall());
+	}
+
+
+
+    private IEnumerator MakeGETCall()
+    {
+        Debug.Log("Inside of Coroutine");
+        while (true)
+        {
+            Debug.Log("Inside of while loop.");
+            download();
+            yield return new WaitForSeconds(2);
+        }
+    }
+
+
+
+    private void download()
+    {
+        Debug.Log("Inside download");
+        UnityWebRequest webRequest = new UnityWebRequest(baseUrl + "/players", UnityWebRequest.kHttpVerbGET);
+        DownloadHandler downloadHandler = new DownloadHandlerBuffer();
+        webRequest.downloadHandler = downloadHandler;
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+        webRequest.Send();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+
+}
